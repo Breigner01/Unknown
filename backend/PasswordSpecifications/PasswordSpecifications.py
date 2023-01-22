6f509439eafd046b9d137f3f8962cd5c5e14df51
Christian Henneveld <cpangia881@gmail.com>
2023-01-22 08:21:13 -0500

    Fixed constructor issue

--- backend/PasswordSpecifications/PasswordSpecifications.py
+++ backend/PasswordSpecifications/PasswordSpecifications.py
@@ -21,3 +21,13 @@ class PasswordSpecifications:
         self.min_numbers = 1
         self.min_symbols = 1
         self.ambiguous = True
+
+    def __init__(self, length: int, lowercase: bool, uppercase: bool, numbers: bool, symbols: bool, min_numbers: int, min_symbols: int, ambiguous: bool):
+        self.length = length
+        self.lowercase = lowercase
+        self.uppercase = uppercase
+        self.numbers = numbers
+        self.symbols = symbols
+        self.min_numbers = min_numbers
+        self.min_symbols = min_symbols
+        self.ambiguous = ambiguous
