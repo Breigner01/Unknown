ca29a2b48bd9f60181cc0be5bb0b85d817b90ca8
Figgueh <31101890+Figgueh@users.noreply.github.com>
2023-01-22 08:27:21 -0500

    Crack time (#3)
    * feat. amdahl's law
    
    * bug fix
    
    * returns whole numbers
    
    * Added CrackTime file and handled check_password_sms
    
    * bug fix
    
    * returns whole numbers
    
    * touched generate_password_sms, but unsure of how to continue.
    
    * returns whole numbers
    
    * touched generate_password_sms, but unsure of how to continue.
    
    * Handeled moores via text

---
+++ backend/CrackTime.py
@@ -0,0 +1,65 @@
+import math
+
+SECONDSINMIN = 60
+SECONDSINHOUR = SECONDSINMIN * 60
+SECONDSINDAY = SECONDSINHOUR * 24 # 86400
+SECONDSINWEEK = SECONDSINDAY * 7
+SECONDSINMONTH = 2592000
+SECONDSINYEAR = SECONDSINDAY * 365 # 31536000
+
+class CrackTime:
+    password = None
+    attempts_per_second = 37085000000   # Follows RTX2080  http://ww16.hivesystem.io/blog/are-your-passwords-in-the-green?sub1=20230122-1210-36bf-8f71-49af03a1c99a
+    # allowed_characters = 88  # 26 lower, 26 upper, 10 digit, 26 special
+    allowed_characters = 62
+    sample_space = None
+    use_moores = False
+
+    def __init__(self, password, use_moores):
+        self.password = password
+        self.sample_space = self.allowed_characters ** len(self.password)
+        self.use_moores = use_moores
+
+    def generate_time(self):
+        return self.sample_space / self.attempts_per_second
+
+    def without_moores(self):
+        if self.generate_time() / SECONDSINYEAR > 1:
+            return str(math.floor(self.generate_time() / SECONDSINYEAR)) + " years"
+        elif self.generate_time() / SECONDSINMONTH > 1:
+            return str(math.floor(self.generate_time() / SECONDSINMONTH)) + " months"
+        elif self.generate_time() / SECONDSINWEEK > 1:
+            return str(math.floor(self.generate_time() / SECONDSINWEEK)) + " weeks"
+        elif self.generate_time() / SECONDSINDAY > 1:
+            return str(math.floor(self.generate_time() / SECONDSINDAY)) + " days"
+        elif self.generate_time() / SECONDSINHOUR > 1:
+            return str(math.floor(self.generate_time() / SECONDSINHOUR)) + " hours"
+        elif self.generate_time() / SECONDSINMIN > 1:
+            return str(math.floor(self.generate_time() / SECONDSINMIN)) + " minutes"
+        elif self.generate_time() > 1:
+            return str(math.floor(self.generate_time())) + " seconds"
+        else:
+            return "instant"
+
+    def with_moores(self, time_in_years, time_to_crack=0):
+        # Returns the number of years it would take to crack the password
+
+        if time_in_years < 2:
+            time_to_crack += time_in_years * SECONDSINYEAR
+            return str(math.floor(time_to_crack / SECONDSINYEAR)) + " years"
+        else:
+            # Remove the sample space completed in the two years.
+            sample_space_completed = self.attempts_per_second * SECONDSINYEAR * 2
+            self.sample_space -= sample_space_completed
+
+            # Recalculate the time needed to complete the rest
+            self.attempts_per_second = self.attempts_per_second * 2
+            time_to_crack += self.sample_space / self.attempts_per_second
+            return self.with_moores(self.generate_time() / SECONDSINYEAR, time_to_crack)
+
+    def show_results(self):
+        time = self.without_moores()
+        if self.use_moores:
+            if "years" in time:
+                return self.with_moores(self.generate_time() / SECONDSINYEAR)
+        return time
--- backend/Sms.py
+++ backend/Sms.py
@@ -1,29 +1,53 @@
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
-from PasswordSpecifications.PasswordSpecifications import PasswordSpecifications
+from PasswordSpecifications import PasswordSpecifications
+from CrackTime import CrackTime
+from PasswordGeneration.PasswordGenerator import PasswordGenerator
 load_dotenv()
 
 
-def parse_message(message):
-    p = PasswordSpecifications()
-    for name in message.split(" "):
-        if name == "-sc":
-            p.special_characters = True
-        elif name == "-n":
-            p.numbers = True
-        # Avoid ambiguous chars if this is set to true
-        elif name == "-a":
-            p.ambiguous = True
-        elif name == "-u":
-            p.uppercase = True
-        elif name == "-l":
-            p.lowercase = True
-        elif name.isnumeric():
-            p.length = int(name)
-        else:
-            pass
-    return p
+class Sms:
+
+    def __init__(self):
+        self.account_sid = os.getenv("account_sid")
+        self.auth = os.getenv("auth")
+        self.message = ""
+        print(self.account_sid)
+        print(self.auth)
+        print("Twilio Initialized")
+
+    def generate_password_sms(self, message):
+        p = self.parse_message(message)
+        #  call your function to generate password pass in p
+        pw_gen = PasswordGenerator()
+        self.message = pw_gen.generate(p)
+        # client = Client(self.account_sid, self.auth)
+        # client.messages.create(
+        #     to=number,
+        #     from_=os.getenv("from_number"),
+        #     body=message
+        # )
+
+    def parse_message(message):
+        p = PasswordSpecifications()
+        for name in message.split(" "):
+            if name == "-sc":
+                p.special_characters = True
+            elif name == "-n":
+                p.numbers = True
+            # Avoid ambiguous chars if this is set to true
+            elif name == "-a":
+                p.ambiguous = True
+            elif name == "-u":
+                p.uppercase = True
+            elif name == "-l":
+                p.lowercase = True
+            elif name.isnumeric():
+                p.length = int(name)
+            else:
+                pass
+        return p
 
 
 class Sms:
@@ -39,7 +63,8 @@ class Sms:
     def generate_password_sms(self, message):
         p = parse_message(message)
         #  call your function to generate password pass in p
-        self.message = "BEN function"
+        pw_gen = PasswordGenerator()
+        self.message = pw_gen.generate(p)
         # client = Client(self.account_sid, self.auth)
         # client.messages.create(
         #     to=number,
@@ -47,5 +72,7 @@ class Sms:
         #     body=message
         # )
 
-    def check_password_sms(self, message):
-        pass
+    def check_password_sms(self, message, use_moores):
+        pw = CrackTime(message, use_moores)
+        return pw.show_results()
+
