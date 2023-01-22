b84093f7ac8086fbe2143df0b2e98236e2b20ca7
Christian Henneveld <cpangia881@gmail.com>
2023-01-22 12:15:04 -0500

    genPass changes

--- backend/Sms.py
+++ backend/Sms.py
@@ -53,7 +53,7 @@ class Sms:
         p = parse_message(message)
         #  call your function to generate password pass in p
         pw_gen = PasswordGenerator()
-        self.message = pw_gen.generate(p)
+        return pw_gen.generate(p)
         # client = Client(self.account_sid, self.auth)
         # client.messages.create(
         #     to=number,
